c5fc3ae155e31eb36c8e5e7d6ca942e37d88c0ef
Sadeq Miah <muhammadluban@googlemail.com>
2026-09-18 16:19:04 +0000

    Added new.tsx to reconstruct project

---
+++ app/new.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function New() {
+  return (
+    <View>
+      <Text>Here, we will try to re-render the index.tsx</Text>  
+    </View>
+  )
+}
+
